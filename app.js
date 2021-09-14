381c1dcc0278436042cea23c1c31a2cfbf1863bd
InSun <68889967+KangInSun@users.noreply.github.com>
2021-09-14 18:06:16 +0900

    [UPDATE] login

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var myClassRouter = require('./routes/myClass');
-var loginStuRouter = require('./routes/loginStu');
+var loginRouter = require('./routes/login');
 var signUpRouter = require('./routes/signUp');
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/myclass', myClassRouter);
-app.use('/loginStu', loginStuRouter);
+app.use('/login', loginRouter);
 app.use('/signUp', signUpRouter);
 
 // catch 404 and forward to error handler
