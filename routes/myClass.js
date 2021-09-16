bee5bee770a52e0756e34a54ac06ad7f8055f6af
민지 <71913069+Minj02@users.noreply.github.com>
2021-09-16 16:58:32 +0900

    makeClass 기능 구현

--- routes/myClass.js
+++ routes/myClass.js
@@ -1,17 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
+var FindMyClass = require("../util/utils");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',async (req, res, next) => {
+  const user = await User.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+  });
+  
+  
+
+  const list = await FindMyClass(user[0].id);
+  console.log(list);
   res.render('myClass',{
     isLogin: req.session.isLogin,
     val: {
       user_name: req.session.name,
-      total_class:"8",
+      total_class:list.length,
       teacher_name:"김은애",
       how_many_stu:"18"
     },
-    list:[{class_name: "dd", teacher_name: "asd", how_many_stu:"dd"}, {class_name: "dd", teacher_name: "asd", how_many_stu:"dd"},]
+    list:list
   });
 });
 
